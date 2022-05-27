7c16c30b3b249f53e54cc3984a935a21e6f44fea
Bogdan-Strat <73583515+Bogdan-Strat@users.noreply.github.com>
2022-05-27 22:58:02 +0300

    Update Main.java

--- src/main/java/com/example/paojdbc/Main.java
+++ src/main/java/com/example/paojdbc/Main.java
@@ -4,6 +4,6 @@ import com.example.paojdbc.repositories.UserRepository;
 
 public class Main {
     public static void main(String[] args) {
-        UserRepository userRepository = new UserRepository();
+        
     }
 }
